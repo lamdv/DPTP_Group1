687e6e2ed19ddcff12b54f3c018127d63a58dbad
nhanntwpv <hostdknhan2@gmail.com>
2016-03-29 05:11:58 +0700

    Grabber init commit

---
+++ Grabber/GrabberMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GrabberMVC5.MvcApplication" Language="C#" %>
---
+++ Grabber/GrabberMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrabberMVC5.Startup))]
+namespace GrabberMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
