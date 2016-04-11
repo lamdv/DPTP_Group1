d9a4fba35589443fef69d162d4640bc6123a968d
lamdv <lam.dv@live.com>
2016-04-12 01:03:53 +0700

    Test generated

---
+++ Grabber/GrabberMVC5test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrabberMVC5.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrabberMVC5.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AboutTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ContactTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
