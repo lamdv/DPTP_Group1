8e7b6176609ec37618aec19e49d8d385b4882310
haanh764 <ngohaanh764@gmail.com>
2016-04-17 21:50:01 +0700

    Added files via upload

---
+++ Grabber/features/support/paths.rb
@@ -0,0 +1,26 @@
+# Maps a name to a path. Used by the
+#   When /^I go to (.+)$/ do |page_name|
+# step definition in tweet_steps.rb
+
+module NavigationHelpers
+    
+  def path_to(page_name)
+    case page_name
+
+    when /^the home\s?page$/
+      '/'
+    
+    else
+      begin
+        page_name =~ /^the (.*) page$/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue NoMethodError, ArgumentError
+        raise "Cannot find mapping from \"#{page_name}\" to a path.\n" +
+          "Go and add a mapping in #{__FILE__}"
+      end
+    end
+  end
+end
+
+World(NavigationHelpers)
